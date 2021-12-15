123c7ccc9294270b1745545a03ad951f149f04c5
sachan72 <61386070+sachan72@users.noreply.github.com>
2021-12-15 20:29:59 +0530

    Update common_variables.tf

--- challenge1/project_resources/common_variables.tf
+++ challenge1/project_resources/common_variables.tf
@@ -10,4 +10,21 @@ variable "project_name" {
   default = "gcp-project-name"
 }
 
+variable "subnetwork" {
+ default = "projects/host-gcp-project-name/regions/europe-west2/subnetworks/host-project-europe-west2" 
+}
+
+variable "subnetwork" {
+ default = "projects/gcp-project-name/regions/europe-west2/subnetworks/service-project-europe-west2" 
+}
+
+variable "network_firewall_tags" {
+  default  = [
+    "tag1",
+    "tag2",
+    "tag3",
+    ]
+}
+
+
 ## and so on .. All common variables which have to be passed for resource creation can be passed from here.
